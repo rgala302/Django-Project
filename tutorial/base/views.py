8814cfa148ba537c53d40377de7248891cd775ff
rgala302 <roberto57.er@gmail.com>
2024-09-15 16:02:42 -0400

    CRUDE

--- tutorial/base/views.py
+++ tutorial/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Room
 
 rooms = [
     {'id':1, 'name':'Lets learn python!'},
@@ -7,13 +8,11 @@ rooms = [
 ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request, 'base/room.html', context)
